53d37d129c01b7fb2b3e11944442187401beda51
artemis-beta <KXZ186@student.bham.ac.uk>
2016-11-02 22:50:35 +0100

    Removed unneeded pickling and added option to add captions

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # noteTeX
 Wouldn't it be great to put all your PDFs into a single directory and then quickly make a report out of them for easy presentation? Well now you can with noteTeX! Create a directory and move all your PDF files to this directory before either calling an instance of `noteTeX`:
 ```
-noteTeX(directory="./",outputfilename="Analysis_Update.tex",filestoignoreList=[],keepBuildFiles=False)
+noteTeX(directory="./",outputfilename="Analysis_Update.tex",captions=[],filestoignoreList=[],keepBuildFiles=False)
 ```
 or simply running within the folder:
 ```
 python /address/to/noteTeX/noteTeX.py
 ```
 
+## Setting Captions
+
+By default noteTeX simply takes the filename to be the caption for the attached PDF, however the user can specify captions as an argument. The ID for each element at definition is an integer starting from 0 assigned to each file when the files are ordered alphabetically. You can view the elements as a table after running the script by:
+```
+report = noteTeX()
+report.printElements()
+```
+
 ## Prerequisites
-For noteTeX to work you require `latexmk` to be installed on your machine. `pickle` is used to create temporary files containing the PDF titles and captions in dictionaries
+For noteTeX to work you require `latexmk` to be installed on your machine. `tabulate` is used to produce the table of elements contained, these are printed using the `printElements()` function.
 
 ## Future Development
 
--- noteTeX.py
+++ noteTeX.py
@@ -1,22 +1,26 @@
 import pickle
 import os
-
+from tabulate import tabulate
 version = "v0.1.0"
 
 preamble = "\\documentclass{article}\n\\usepackage{graphicx}\n\\usepackage{cprotect}\n\\begin{document}\n"
 end = "\\end{document}\n"
 
 class noteTeX:
-	def __init__(self,directory='./',outputfile="Analysis_Update.tex",exception_list=[],keepBuildFiles=False):
+	def __init__(self,directory='./',outputfile="Analysis_Update.tex",captions=[],exception_list=[],keepBuildFiles=False):
 		self.directory = directory
 		self.exceptions = exception_list
 		self.file = open('%s' % outputfile,'wb')
 		self.exceptions.append(outputfile)
+		self.exceptions.append(outputfile.replace(".tex",".pdf"))
+		self.elements = {}
 		self.kBF = keepBuildFiles
 		self.filest = outputfile
+		self.captions = captions
 	
 	def autoGenPDF(self):
-		for filename in os.listdir(self.directory):
+		ID = 0
+		for filename in sorted(os.listdir(self.directory)):
 			autocapt = "\\verb|%s|" % filename
 			if filename in self.exceptions:
 				print "Ignoring file %s which is an exception" % filename
@@ -25,9 +29,12 @@ class noteTeX:
 				print "Adding file %s to report" % filename
 				dict_ = {}
 				dict_['IMAGE'] = filename
-				dict_['CAPTION'] = autocapt
-				pickle.dump(dict_,open(filename.replace('.pdf','')+".ltxobj",'w'))
-
+				if len(self.captions) <= ID or self.captions[ID] == "":
+					dict_['CAPTION'] = autocapt
+				else:
+					dict_['CAPTION'] = self.captions[ID]
+				self.elements[ID] = dict_
+			ID += 1
 	def addTeXGraphic(self,image,caption):
 		self.file.write("\\begin{{figure}}\n\\cprotect\\caption{{{caption}}}\n".format(caption=caption))
 		self.file.write("\n\\includegraphics[width=\\textwidth]{{{image}}}\n".format(image=image))
@@ -35,44 +42,45 @@ class noteTeX:
 
 #	def addTeXFile(self):
 
-	def readPDFtoDict(self):
-
-		dict_rep = []
-	
-		for filename in os.listdir(self.directory):
-
-			if filename.endswith(".ltxobj"):
-				x = pickle.load(open(filename,'rb'))
-				assert x,"ERROR: Failed to Load File Object from %s" % filename
-				dict_rep.append(x)
-		return dict_rep
 
 	def assembleReport(self):
 		
-		dict_rep = self.readPDFtoDict()
 		self.file.write(preamble)
-		for element in dict_rep:
-			assert element,"ERROR: Failed to Load Dictionary Item"
-			self.addTeXGraphic(element['IMAGE'],element['CAPTION'])
+		for ID in self.elements:
+			assert self.elements[ID],"ERROR: Failed to Load Dictionary Item"
+			self.addTeXGraphic(self.elements[ID]['IMAGE'],self.elements[ID]['CAPTION'])
 		self.file.write(end)
 	
 	def removeFiles(self,filename):
-		files_to_delete = [".aux",".ltxobj",".log",".cpt",".fls",".fdb_latexmk"]
+		files_to_delete = [".aux",".log",".cpt",".fls",".fdb_latexmk"]
 		for ending in files_to_delete:
 			if filename.endswith(ending):
 				return True
 		return False
 
+	def printElements(self):
+		table = []
+		for ID in self.elements:
+
+			table.append([ID,self.elements[ID]['IMAGE'],self.elements[ID]['CAPTION']])
+
+		print tabulate(table,headers=["ID","Image File","Caption"],tablefmt="fancy_grid")
+
+
+	def compileLaTeX(self):
+	
+		build_command = "latexmk -pdf {file_}".format(file_=self.filest)
+		print "Running LaTeX compiler as: %s" % build_command
+		os.system(build_command)
+	
 	def build(self):
 		print "WELCOME TO NOTETEX %s\n--------------------------\n" % version			
 		self.autoGenPDF()
 		print "Building Report......"
 		self.assembleReport()
 		self.file.close()
-		build_command = "latexmk -pdf {file_}".format(file_=self.filest)
-		print "Running LaTeX compiler as: %s" % build_command
 		print "\n--------------------------\n--------------------------\n\n"
-		os.system(build_command)
+		self.compileLaTeX()
 		build_success = False
 		for filename in os.listdir(self.directory):	
 			if not self.kBF and self.removeFiles(filename):
@@ -86,5 +94,6 @@ class noteTeX:
 
 if __name__ == "__main__":
 
-	report = noteTeX()
+	report = noteTeX(captions=["1","2","3","4","5","6"])
 	report.build()
+	report.printElements()
